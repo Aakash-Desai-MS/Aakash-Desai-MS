7f134cbb6898372d9a5d0a0b0485dceab7f3e24c
Aakash-Desai-MS <129723629+Aakash-Desai-MS@users.noreply.github.com>
2023-09-14 12:51:55 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,26 +5,24 @@ variable "virtual_machine_vmaccess_extensions" {
     virtual_machine_name      = string
     vm_resource_group_name    = string
     type_handler_version      = optional(string)
-    infra_keyvault_name             = string
-    kv_resource_group               = string
+    infra_keyvault_name       = string
+    kv_resource_group         = string
     username                  = string
     password_secret_name      = string
-    reset_ssh                 = bool
-    ssh_key_secret_name       = string
     remove_user               = string
   }))
 
 default = {
   "key" = {
-    name                  = "reset1"
+    name                 = "reset1"
     password_secret_name = "testpass"
-    remove_user = ""
-    reset_rdp = true
-    infra_keyvault_name             = "infrakv011"
-    kv_resource_group               = "vm-rg"
-    rdp_key_secret_name = "rdpkey-pubkey"
+    remove_user          = ""
+    reset_rdp            = true
+    infra_keyvault_name  = "infrakv011"
+    kv_resource_group    = "vm-rg"
+    rdp_key_secret_name  = "rdpkey-pubkey"
     type_handler_version = "1.5"
-    username = "shyam121"
+    username             = "shyam121"
     virtual_machine_name = "testvm"
     vm_resource_group_name = "vm-rg"
   }
