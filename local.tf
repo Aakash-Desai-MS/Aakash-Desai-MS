cd101fe4f922349d357a3f3db0ec98af533f6b64
Aakash-Desai-MS <129723629+Aakash-Desai-MS@users.noreply.github.com>
2023-09-14 13:03:44 +0530

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,6 +1,1 @@
-locals {
-    reset_password    = { for k, v in var.virtual_machine_vmaccess_extensions : k => v if lookup(v, "reset_rdp", false) == false }
-    reset_rdp    = { for k, v in var.virtual_machine_vmaccess_extensions : k => v if lookup(v, "reset_rdp", false) == true }
-    
-    # ssh_key = local.reset_ssh == true ? lookup(data.azurerm_key_vault_secret.adminssh, each.key)["value"] : null
-}
+
